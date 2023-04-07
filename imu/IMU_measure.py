df30459d6ddfc196bd12b4a6df009ab95f04ddb5
lol2331 <114163487+lol2331@users.noreply.github.com>
2023-04-08 02:09:51 +0300

    feat: added the sensor data to a spreadsheet

--- imu/IMU_measure.py
+++ imu/IMU_measure.py
@@ -1,6 +1,13 @@
 import smbus
 import time
+from openpyxl import load_workbook
+import datetime
 import math
+from datetime import date
+import os
+import sys
+
+import dateutil.utils
 
 # MPU-6050 register addresses
 PWR_MGMT_1 = 0x6B
@@ -17,7 +24,7 @@ GYRO_ZOUT_H = 0x47
 
 # Define constants for unit conversion
 ACCEL_SCALE = 16384.0  # LSB/g
-GYRO_SCALE = 131.0     # LSB/(deg/s)
+GYRO_SCALE = 131.0  # LSB/(deg/s)
 
 # Initialize I2C bus and MPU-6050
 bus = smbus.SMBus(1)
@@ -26,6 +33,11 @@ bus.write_byte_data(0x68, PWR_MGMT_2, 0)
 bus.write_byte_data(0x68, CONFIG, 0x03)
 bus.write_byte_data(0x68, GYRO_CONFIG, 0x10)
 bus.write_byte_data(0x68, ACCEL_CONFIG, 0x10)
+
+wb = load_workbook('C:\\Users\\Andrei\\OneDrive\\Documente\\imu\\imu.xlsx')
+sheet1 = wb["Sheet1"]
+sheet2 = wb["Sheet2"]
+
 
 # Function to read 16-bit word from I2C device
 def read_word_2c(addr):
@@ -38,24 +50,44 @@ def read_word_2c(addr):
     else:
         return val
 
+
 # Function to convert raw accelerometer values to units of g
 def convert_accel(raw):
     return raw / ACCEL_SCALE
+
 
 # Function to convert raw gyroscope values to units of degrees per second
 def convert_gyro(raw):
     return raw / GYRO_SCALE
 
-# Read and print accelerometer and gyroscope data
-while True:
-    accel_x = convert_accel(read_word_2c(ACCEL_XOUT_H))
-    accel_y = convert_accel(read_word_2c(ACCEL_YOUT_H))
-    accel_z = convert_accel(read_word_2c(ACCEL_ZOUT_H))
-    gyro_x = convert_gyro(read_word_2c(GYRO_XOUT_H))
-    gyro_y = convert_gyro(read_word_2c(GYRO_YOUT_H))
-    gyro_z = convert_gyro(read_word_2c(GYRO_ZOUT_H))
 
-    print("accelerometer (x,y,z): ({:.2f}g,{:.2f}g,{:.2f}g)".format(accel_x, accel_y, accel_z))
-    print("gyroscope (x,y,z): ({:.2f}deg/s,{:.2f}deg/s,{:.2f}deg/s)".format(gyro_x, gyro_y, gyro_z))
+try:
+    # Read and print accelerometer and gyroscope data
+    while True:
+        accel_x = convert_accel(read_word_2c(ACCEL_XOUT_H))
+        accel_y = convert_accel(read_word_2c(ACCEL_YOUT_H))
+        accel_z = convert_accel(read_word_2c(ACCEL_ZOUT_H))
+        gyro_x = convert_gyro(read_word_2c(GYRO_XOUT_H))
+        gyro_y = convert_gyro(read_word_2c(GYRO_YOUT_H))
+        gyro_z = convert_gyro(read_word_2c(GYRO_ZOUT_H))
 
-    time.sleep(0.1)
+        now = datetime.datetime.now().time()
+
+        print(now)
+        print("accelerometer (x,y,z): ({:.2f}g,{:.2f}g,{:.2f}g)".format(accel_x, accel_y, accel_z))
+        print("gyroscope (x,y,z): ({:.2f}deg/s,{:.2f}deg/s,{:.2f}deg/s)".format(gyro_x, gyro_y, gyro_z))
+
+        row1 = (now, accel_x, accel_y, accel_z)
+        row2 = (now, gyro_x, gyro_y, gyro_z)
+        sheet1.append(row1)
+        sheet1.append(row2)
+        sheet2.append(sys.getsizeof(row1))
+        sheet2.append(sys.getsizeof(row2))
+
+        wb.save('C:\\Users\\Andrei\\OneDrive\\Documente\\imu\\imu.xlsx')
+        time.sleep(5)
+
+
+finally:
+    wb.save('C:\\Users\\Andrei\\OneDrive\\Documente\\imu\\imu.xlsx')
+    print("Goodbye:)")
